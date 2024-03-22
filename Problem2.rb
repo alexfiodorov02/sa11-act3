287c323220d16e3f9b3b3f412bfc58f6ba709606
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-03-22 15:59:36 -0500

    Finished Problem 2

--- Problem2.rb
+++ Problem2.rb
@@ -1,4 +1,8 @@
 def parse_invoices(invoice_data)
+  invoice_data.each_line do |line|
+    date, invoice_number, client_name, amount = line.scan(/(\d{4}-\d{2}-\d{2}) - (\w+) - ([\w\s]+) - (\$\d+)/).flatten
+    puts "Date: #{date}, Invoice Number: #{invoice_number}, Client: #{client_name}, Amount: #{amount}"
+  end
 end
 
 invoice_entries = <<-INVOICES
