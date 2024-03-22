06f64a71554cf70d4f469a49375eabe7b187ac5c
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-03-22 15:44:19 -0500

    Added puts method to Problem 1

--- Problem1.rb
+++ Problem1.rb
@@ -6,7 +6,7 @@ def extract_urls(text)
       [-a-z0-9+&@#/%?=~_|!:,.;]*[-a-z0-9+&@#/%=~_|]
     )
   }ix
-  text.scan(url_regex).map(&:first)
+  puts(text.scan(url_regex).map(&:first))
 end
 
 sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
